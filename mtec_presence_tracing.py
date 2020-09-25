e20883a232ac56a0050ee1359476d65b1c4a546d
45635700 <moritzhoferer@gmail.com>
2020-09-25 17:52:24 +0200

    Add end message

--- mtec_presence_tracing.py
+++ mtec_presence_tracing.py
@@ -188,6 +188,8 @@ def main() -> None:
     if os.path.exists('./geckodriver.log'):
         os.remove('./geckodriver.log')  
 
+    print('Successfully fill and submitted D-MTEC Presence Tracing form!')
+
 
 if __name__ == '__main__':
     main()
