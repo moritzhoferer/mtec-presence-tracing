b55f47549f918b96ae8528a5e1bdbe9fdc3a4170
45635700 <moritzhoferer@gmail.com>
2020-09-26 18:19:44 +0200

    Define file input

--- add_alias.sh
+++ add_alias.sh
@@ -2,15 +2,13 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+FILEINPUT="# MTEC presence tracing\nalias mtec-presence-tracing='python3 $SCRIPTPATH/mtec_presence_tracing.py '\n\n#MTEC presence tracing - setup\nalias mtec-setup='python3 $SCRIPTPATH/setup.py'\n"
+
 if [ -f ~/.bash_aliases ]; then
-echo "# MTEC presence tracing
-alias mtec-presence-tracing='python3 $SCRIPTPATH/mtec_presence_tracing.py '
-" >> ~/.bash_aliases
+echo  -e $FILEINPUT >> ~/.bash_aliases
 echo "Added to ~/.bash_aliases"
 else if [ -f ~/.bashrc ]; then
-echo "# MTEC presence tracing
-alias mtec-presence-tracing='python3 $SCRIPTPATH/mtec_presence_tracing.py '
-" >> ~/.bashrc
+echo -e $FILEINPUT >> ~/.bashrc
 echo "Added to ~/.bashrc"
 else
 echo ".bashrc and .bash_aliases do not exist."
